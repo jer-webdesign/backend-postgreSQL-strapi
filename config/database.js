f2a228ee8e5b38bd460a3096dbf175d8ab1434c3
Jerry <jeremy.g.olanda@gmail.com>
2025-06-18 21:39:20 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,17 +1,24 @@
 const path = require('path');
 
-// module.exports = ({ env }) => {
-//   const client = env('DATABASE_CLIENT', 'sqlite');
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
       host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
+      port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL') ? { rejectUnauthorized: false } : false,
+      ssl: env.bool('DATABASE_SSL', true) && {
+        rejectUnauthorized: false, // Supabase doesn't use self-signed certs, so this is safe
+      },
+      schema: env('DATABASE_SCHEMA', 'public'),
     },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    acquireConnectionTimeout: 60000,
   },
 });
+
