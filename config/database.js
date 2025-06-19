96c5cea5853e53f50e2d691309e1108cef74c985
Jerry <jeremy.g.olanda@gmail.com>
2025-06-18 19:27:28 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,20 @@
 const path = require('path');
 
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+// module.exports = ({ env }) => {
+//   const client = env('DATABASE_CLIENT', 'sqlite');
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', true),
+    },
+  },
+});
 
   const connections = {
     mysql: {
