e960a2fecf14788b3c2dafcaff62e7c062c04952
Jerry <jeremy.g.olanda@gmail.com>
2025-06-18 21:48:49 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,21 +4,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
+      host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
+      database: env('DATABASE_NAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true) && {
-        rejectUnauthorized: false, // Supabase doesn't use self-signed certs, so this is safe
-      },
-      schema: env('DATABASE_SCHEMA', 'public'),
+      ssl: env.bool('DATABASE_SSL') ? { rejectUnauthorized: false } : false,
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    acquireConnectionTimeout: 60000,
   },
 });
+
 
