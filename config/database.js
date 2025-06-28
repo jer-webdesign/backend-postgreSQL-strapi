10a91d0f16eee4845717300fc4e2ce7753b69203
Jerry <jeremy.g.olanda@gmail.com>
2025-06-27 19:32:26 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -11,6 +11,10 @@ module.exports = ({ env }) => ({
       password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL') ? { rejectUnauthorized: false } : false,
     },
+    pool: {
+      min: 2,
+      max: 10,   // increase max connections if needed
+    },    
   },
 });
 
