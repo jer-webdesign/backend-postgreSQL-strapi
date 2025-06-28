eb646f912404d21d77cedffa3dd60c171fe90d20
Jerry <jeremy.g.olanda@gmail.com>
2025-06-27 19:03:10 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', 'wumupngcsqjcnixorxoi.supabase.co'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'postgres'),
       user: env('DATABASE_USERNAME', 'postgres'),
